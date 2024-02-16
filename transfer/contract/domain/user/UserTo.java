570412e60abdde25f7499203b4feb3830ae47136
DaPiankov <DaPiankov@croc.ru>
2024-02-16 10:26:45 +0300

    [feat]: Добавил класс сущности пользователя

--- transfer/contract/domain/user/UserTo.java
+++ transfer/contract/domain/user/UserTo.java
@@ -1,11 +1,14 @@
 package transfer.contract.domain.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import heavenboards.user.service.user.UserRole;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
+import java.time.ZonedDateTime;
 import java.util.UUID;
 
 /**
@@ -14,11 +17,52 @@ import java.util.UUID;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder(setterPrefix = "with", toBuilder = true)
+@Builder(toBuilder = true)
 @Accessors(chain = true)
 public class UserTo {
     /**
      * Идентификатор.
      */
     private UUID id;
+
+    /**
+     * Уникальное имя пользователя.
+     */
+    private String username;
+
+    /**
+     * Хэшированный пароль.
+     */
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    /**
+     * Роль.
+     */
+    private UserRole role;
+
+    /**
+     * Имя.
+     */
+    private String firstName;
+
+    /**
+     * Фамилия.
+     */
+    private String lastName;
+
+    /**
+     * Признак "Удален" (для soft-delete).
+     */
+    private boolean deleted;
+
+    /**
+     * Дата и время создания.
+     */
+    private ZonedDateTime createdAt;
+
+    /**
+     * Дата и время последнего обновления.
+     */
+    private ZonedDateTime updatedAt;
 }
