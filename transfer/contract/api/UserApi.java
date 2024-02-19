9f9e811823083baec05ba06b8626a39410f43ec0
DaPiankov <DaPiankov@croc.ru>
2024-02-19 12:33:35 +0300

    [feat]: Добавил эндпоинт для получения всех пользователей по ids

--- transfer/contract/api/UserApi.java
+++ transfer/contract/api/UserApi.java
@@ -4,7 +4,12 @@ import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import transfer.contract.domain.user.UserTo;
+
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 /**
  * Api-клиент для сервиса пользователей.
@@ -27,4 +32,13 @@ public interface UserApi {
      */
     @GetMapping(value = "/api/v1/user/{username}", produces = "application/json")
     UserTo findUserByUsername(@PathVariable String username);
+
+    /**
+     * Найти всех пользователей по идентификаторам.
+     *
+     * @param ids - идентификаторы
+     * @return пользователи
+     */
+    @GetMapping(value = "/api/v1/user", produces = "application/json")
+    List<UserTo> findAllByIds(final @RequestBody Set<UUID> ids);
 }
