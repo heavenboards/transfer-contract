a253c3fa5d9161b61d8efd50b9632c4a27dabec9
Pyankov Daniil <109591434+pyankovdaniil@users.noreply.github.com>
2024-02-19 15:47:54 +0300

    [feat]: Добавил эндпоинты в ProjectApi (#2)
    Co-authored-by: DaPiankov <DaPiankov@croc.ru>

--- transfer/contract/api/ProjectApi.java
+++ transfer/contract/api/ProjectApi.java
@@ -2,6 +2,13 @@ package transfer.contract.api;
 
 import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import transfer.contract.domain.project.ProjectOperationResultTo;
+import transfer.contract.domain.project.ProjectTo;
+
+import java.util.List;
 
 /**
  * Api-клиент для сервиса проектов.
@@ -15,4 +22,20 @@ import org.springframework.cloud.openfeign.FeignClient;
     url = "${microservice.project-api.url}"
 )
 public interface ProjectApi {
+    /**
+     * Получить все проекты пользователя.
+     *
+     * @return все проекты пользователя
+     */
+    @GetMapping(value = "/api/v1/project", produces = "application/json")
+    List<ProjectTo> getAllProjects();
+
+    /**
+     * Создать проект.
+     *
+     * @param project - данные проекта.
+     * @return результат операции создания
+     */
+    @PostMapping(value = "/api/v1/project", produces = "application/json")
+    ProjectOperationResultTo createProject(final @RequestBody ProjectTo project);
 }
