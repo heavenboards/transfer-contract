f5d45e744ef8d6952b7da61ed90ac582bc594769
DaPiankov <DaPiankov@croc.ru>
2024-03-01 20:30:03 +0300

    [feat]: Добавил эндпоинт для поиска проекта по id

--- transfer/contract/api/ProjectApi.java
+++ transfer/contract/api/ProjectApi.java
@@ -3,12 +3,14 @@ package transfer.contract.api;
 import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import transfer.contract.domain.project.ProjectOperationResultTo;
 import transfer.contract.domain.project.ProjectTo;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Api-клиент для сервиса проектов.
@@ -22,6 +24,15 @@ import java.util.List;
     url = "${microservice.project-api.url}"
 )
 public interface ProjectApi {
+    /**
+     * Поиск проекта по идентификатору.
+     *
+     * @param id - идентификатор проекта
+     * @return данные проекта
+     */
+    @GetMapping(value = "/api/v1/project/{id}", produces = "application/json")
+    ProjectTo findProjectById(final @PathVariable UUID id);
+
     /**
      * Получить все проекты пользователя.
      *
