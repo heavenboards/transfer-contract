f7fff14c16a3465c80d68ea15f749f2e8701ddd1
DaPiankov <DaPiankov@croc.ru>
2024-03-02 15:08:52 +0300

    [feat]: Добавил эндпоинт для создания группы задач

--- transfer/contract/api/GroupApi.java
+++ transfer/contract/api/GroupApi.java
@@ -2,6 +2,10 @@ package transfer.contract.api;
 
 import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import transfer.contract.domain.group.GroupOperationResultTo;
+import transfer.contract.domain.group.GroupTo;
 
 /**
  * Api-клиент для взаимодействия с группами задач.
@@ -15,4 +19,12 @@ import org.springframework.cloud.openfeign.FeignClient;
     url = "${microservice.task-api.url}"
 )
 public interface GroupApi {
+    /**
+     * Создать группу задач.
+     *
+     * @param group - to-модель группы задач
+     * @return результат операции создания группы задач
+     */
+    @PostMapping(value = "/api/v1/group", produces = "application/json")
+    GroupOperationResultTo createGroup(final @RequestBody GroupTo group);
 }
---
+++ transfer/contract/domain/group/GroupOperationErrorCode.java
@@ -0,0 +1,11 @@
+package transfer.contract.domain.group;
+
+/**
+ * Код ошибки при операции над группой задач.
+ */
+public enum GroupOperationErrorCode {
+    /**
+     * Группа задач с таким названием уже существует на доске.
+     */
+    GROUP_NAME_ALREADY_EXIST_IN_BOARD
+}
