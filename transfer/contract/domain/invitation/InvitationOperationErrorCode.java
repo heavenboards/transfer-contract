28e13dacbe5b5f1bdb6d13a7e41e0652e8859a66
DaPiankov <DaPiankov@croc.ru>
2024-03-03 22:37:34 +0300

    [feat]: Добавил тесты для эндпоинта создания приглашения пользователя в проект

--- transfer/contract/domain/invitation/InvitationOperationErrorCode.java
+++ transfer/contract/domain/invitation/InvitationOperationErrorCode.java
@@ -3,5 +3,9 @@ package transfer.contract.domain.invitation;
 /**
  * Код ошибки при операции над приглашением.
  */
-public class InvitationOperationErrorCode {
+public enum InvitationOperationErrorCode {
+    /**
+     * Приглашение этому пользователю в этот проект уже отправлено.
+     */
+    INVITATION_ALREADY_CREATED
 }
