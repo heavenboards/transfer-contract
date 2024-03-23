2c6e8e99bf9183fb86a1cc4bc71439d9942636e5
DaPiankov <DaPiankov@croc.ru>
2024-03-23 20:22:35 +0300

    [fix]: Поправил логику работы класса AuthForwardingRequestInterceptor

--- transfer/contract/config/AuthForwardingRequestInterceptor.java
+++ transfer/contract/config/AuthForwardingRequestInterceptor.java
@@ -10,26 +10,31 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import java.util.Collection;
 import java.util.Optional;
 
+/**
+ * Класс для передачи хедеров при взаимодействии микросервисов через feign-клиенты.
+ */
 @Slf4j
 public class AuthForwardingRequestInterceptor implements RequestInterceptor {
-
+    /**
+     * Метод обогащения запроса хедером AUTHORIZATION.
+     *
+     * @param requestTemplate - запрос
+     */
     @Override
     public void apply(RequestTemplate requestTemplate) {
         Collection<String> headerTokens = requestTemplate.headers().get(HttpHeaders.AUTHORIZATION);
         if (headerTokens == null || headerTokens.isEmpty()) {
-            return;
-        }
-        String auth = Optional.ofNullable(RequestContextHolder.getRequestAttributes())
+            String authorization = Optional.ofNullable(RequestContextHolder.getRequestAttributes())
                 .map(ServletRequestAttributes.class::cast)
                 .map(ServletRequestAttributes::getRequest)
                 .map(req -> req.getHeader(HttpHeaders.AUTHORIZATION))
                 .orElse(null);
 
-        if (auth != null) {
-            requestTemplate.header(HttpHeaders.AUTHORIZATION, auth);
-        } else {
-            log.error("no auth ((((((");
+            if (authorization != null) {
+                requestTemplate.header(HttpHeaders.AUTHORIZATION, authorization);
+            } else {
+                log.error("No Authorization header found in current request context!");
+            }
         }
-
     }
 }
--- transfer/contract/config/FeignClientConfig.java
+++ transfer/contract/config/FeignClientConfig.java
@@ -17,23 +17,34 @@ import transfer.contract.api.ProjectApi;
 
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Класс для создания сконфигурированных бинов feign-клиентов.
+ */
 @Configuration
 @RequiredArgsConstructor
 public class FeignClientConfig {
-
+    /**
+     * Класс для записи и конвертации объектов в JSON.
+     */
     private final ObjectMapper objectMapper;
 
+    /**
+     * Конфигурация feign-клиента для сервиса проектов.
+     *
+     * @param baseUrl - url сервиса проектов
+     * @return сконфигурированный бин для взаимодействия с сервисом проектов
+     */
     @Bean
     public ProjectApi projectApi(@Value("${microservice.project-api.url}") String baseUrl) {
         return Feign.builder()
-                .client(new OkHttpClient())
-                .requestInterceptor(new AuthForwardingRequestInterceptor())
-                .decoder(new JacksonDecoder(objectMapper))
-                .encoder(new JacksonEncoder(objectMapper))
-                .logger(new Slf4jLogger(ProjectApi.class))
-                .logLevel(Logger.Level.NONE)
-                .retryer(Retryer.NEVER_RETRY)
-                .options(new Request.Options(15, TimeUnit.MINUTES, 15, TimeUnit.MINUTES, true))
-                .target(ProjectApi.class, baseUrl);
+            .client(new OkHttpClient())
+            .requestInterceptor(new AuthForwardingRequestInterceptor())
+            .decoder(new JacksonDecoder(objectMapper))
+            .encoder(new JacksonEncoder(objectMapper))
+            .logger(new Slf4jLogger(ProjectApi.class))
+            .logLevel(Logger.Level.NONE)
+            .retryer(Retryer.NEVER_RETRY)
+            .options(new Request.Options(15, TimeUnit.MINUTES, 15, TimeUnit.MINUTES, true))
+            .target(ProjectApi.class, baseUrl);
     }
 }
